15d8b6ba9644c4620692def9c5365618bb64fe30
AMF112 <adrianmiguelflores512@gmail.com>
2026-06-19 15:00:58 +0800

    feat: add day 5 completed challenge

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useTaskStore } from '../stores/taskStore.js'
 import HomeView from '../day3/HomeView.vue'
 import TaskListView from '../day4/TaskListView.vue'
+import TodoListView from '../day5/TodoListView.vue'
 
 const routes = [
   // TODO 3: Add a redirect from '/' to '/home'
@@ -11,7 +12,11 @@ const routes = [
   // TODO 4: Add the /home route
   { path: '/home', component: HomeView },
 
-  {path: '/day4/task-list', component: TaskListView}
+  // day 4 path
+  {path: '/day4/task-list', component: TaskListView},
+
+  // day 5 path
+  {path: '/day5/todo-list', component: TodoListView},
 ]
 
 const router = createRouter({
